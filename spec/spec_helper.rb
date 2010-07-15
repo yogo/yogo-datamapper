05daba80dacb20ba0485a773fd002f78ef7ec692
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-15 14:01:06 -0600

    Updating spec_helper to migrate models

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,19 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'spec'
 require 'spec/autorun'
+require 'dm-migrations'
 require 'yogo/project'
+
+require 'ruby-debug'
 
 Spec::Runner.configure do |config|
   config.before(:suite) do
-    @default = DataMapper.setup(:default, "sqlite3::memory:///yogo")
-    @data = DataMapper.setup(Yogo::Project.default_data_repository_name, 
-                                    "persevere://localhost:8080")
+    DataMapper.finalize
+    @default = DataMapper.setup(:default, "sqlite3::memory:")
+    # @data = DataMapper.setup(Yogo::Project.default_data_repository_name, 
+    #                                     "persevere://localhost:8080")
+    
+    Yogo::Project.auto_migrate!
+    Yogo::Project::Collection.auto_migrate!
   end
 end
