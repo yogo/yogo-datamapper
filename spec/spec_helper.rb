40115626ee518ec701a0e2b5ec3211bf98a18b83
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-15 10:36:14 -0600

    setup default adapters for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'yogo_project'
+
+
 require 'spec'
 require 'spec/autorun'
+require 'yogo/project'
 
 Spec::Runner.configure do |config|
-  
+  config.before(:suite) do
+    @default = DataMapper.setup(:default, "sqlite3::memory:///yogo")
+    @data = DataMapper.setup(Yogo::Project.default_data_repository_name, 
+                                    "persevere://localhost:8080")
+  end
 end
