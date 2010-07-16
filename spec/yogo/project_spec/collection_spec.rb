13141afea1ccea454209b58386e0c88626a593c9
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-16 10:22:45 -0600

    spec-ing collection data-model

--- spec/yogo/project_spec/collection_spec.rb
+++ spec/yogo/project_spec/collection_spec.rb
@@ -8,20 +8,18 @@ describe "Yogo::Project::Collection" do
   
   describe "instance" do
     before(:all) do
-      @project = Yogo::Project.create(:name => "A Test Project")
     end
     
     before(:each) do
+      @project = Yogo::Project.create(:name => "A Test Project")
       @collection = @project.collections.create(:name => "CercalDB")
     end
     
     after(:each) do
       @project.collections.destroy
-    end
-    
-    after(:all) do
       @project.destroy
     end
+    
     
     it "should have a name" do
       @collection.name.should be_a_kind_of(String)
@@ -51,19 +49,66 @@ describe "Yogo::Project::Collection" do
       @project.should have(1).collections
     end
     
-    it "should have a schema"
+    it "should have a data_repository" do
+      @project.should respond_to(:data_repository)
+    end
+    
+    describe "data_repository" do
+      it "should be a datamapper respository" do
+        @project.data_repository.should be_a_kind_of(DataMapper::Repository)
+      end
+    end
+    
     it "should have data" do
       @collection.should respond_to(:data)
     end
     
     describe "data" do
-      it "should be a datamapper model" do
-        @collection.data.should be_a_kind_of(DataMapper::Model)
+      before(:each) do
+        @data = @collection.data
       end
       
-      it "should use the project data_repository" do
-        data = @collection.data
-        data.repository.should == @project.data_repository
+      it "should be a datamapper model" do
+        @data.should be_a_kind_of(DataMapper::Model)
+      end
+      
+      it "should use the @collection data_repository" do
+        @data.repository.should == @collection.data_repository
+      end
+      
+      describe "with definition" do        
+        describe "dumping" do
+          it "should have dump_definition method" do
+            @data.should respond_to(:dump_definition)
+          end
+          
+          it "should return a Hash from dump_definition" do
+            @data.dump_definition.should be_a_kind_of(Hash)
+          end
+          
+          it "should return a dump_definition with an added property" do
+            prop_name = :test_property
+            prop_type = String
+            prop_opts = {}
+            
+            @data.property(:test_property, String, prop_opts)
+            
+            dump = @data.dump_definition
+            dumped_props = dump[:properties]
+            dumped_props.size.should == 1
+            
+            dumped_prop = dumped_props.first
+            dumped_prop[:name].should == prop_name
+            dumped_prop[:type].should == prop_type.name
+            dumped_prop[:options].should == prop_opts
+          end
+        end
+        
+        describe "loading" do
+          it "should have load_definition method" do
+            @data.should respond_to(:load_definition)
+          end
+        end
       end
     end
   end
